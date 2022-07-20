ba552f5487b6e66eeba58c81306b42f89db34943
chadokada <chadokada@gmail.com>
2022-07-19 23:16:19 -1000

    added App class and Info class

---
+++ src/components/Info.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Info extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div>
+        This is the info section
+      </div>
+    )
+  }
+}
+
+export default Info;
