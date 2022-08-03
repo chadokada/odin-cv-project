84db752a54ecc559c5fc0e733e9603aa3e657295
chadokada <chadokada@gmail.com>
2022-08-02 22:31:46 -1000

    Created state for Experience

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import React, { Component } from 'react';
-import Info from './components/Info'
+import Info from './components/Info';
+import Experience from "./components/Experience";
+import uniqid from 'uniqid';
 
 class App extends Component {
   constructor(){
@@ -14,7 +16,47 @@ class App extends Component {
         email: 'johndoe@email.com',
         linkedIn: 'johndoe123',
         gitHub: 'johndoe123',
-      }
+      },
+      experience: {
+        company: '',
+        title: '',
+        start: '',
+        end: '',
+        description: '' 
+      },
+      experiences: [
+        {
+          company: "Satriale's Pork Store",
+          title: 'Senior Meat Associate',
+          start: 'January 1999',
+          end: 'June 2007',
+          description: `Cut meat, talk to customers, take it ease, 
+                        cut meat, talk to customers, take it ease,
+                        cut meat, talk to customers, take it ease,
+                        cut meat, talk to customers, take it ease,
+                        cut meat, talk to customers, take it ease,
+                        cut meat, talk to customers, take it ease,
+                        cut meat, talk to customers, take it ease
+                        `,
+          id: uniqid(),
+        },
+        {
+          company: "Satriale's Pork Store",
+          title: 'Senior Meat Associate',
+          start: 'January 1999',
+          end: 'June 2007',
+          description: `Cut meat, talk to customers, take it ease, 
+                        cut meat, talk to customers, take it ease,
+                        cut meat, talk to customers, take it ease,
+                        cut meat, talk to customers, take it ease,
+                        cut meat, talk to customers, take it ease,
+                        cut meat, talk to customers, take it ease,
+                        cut meat, talk to customers, take it ease
+                        `,
+          id: uniqid(),
+        }
+      ]
+
     }
   }
 
@@ -32,93 +74,92 @@ class App extends Component {
 
 
   render(){
-    const {info} = this.state;
+    const {info, experiences} = this.state;
 
     return (
       <div className="resume-container">
 
         <div className="resume-inputs">
 
-        <fieldset className="personal-info">
-          <legend>Personal Information</legend>
-            <p>
-              <label htmlFor="name">Name:</label>
-              <input 
-                onChange={this.handleInfoChange}
-                type="text" 
-                id="name"
-                value={info.name}
-              />
-            </p>
+          <fieldset className="personal-info">
+            <legend>Personal Information</legend>
+              <p>
+                <label htmlFor="name">Name:</label>
+                <input 
+                  onChange={this.handleInfoChange}
+                  type="text" 
+                  id="name"
+                  value={info.name}
+                />
+              </p>
 
-            <p>
-              <label htmlFor="address1">Address:</label>
-              <input 
-                onChange={this.handleInfoChange}
-                type="text" 
-                id="address1"
-                value={info.address1}
-              />
-            </p>
+              <p>
+                <label htmlFor="address1">Address:</label>
+                <input 
+                  onChange={this.handleInfoChange}
+                  type="text" 
+                  id="address1"
+                  value={info.address1}
+                />
+              </p>
 
-            <p>
-              <label htmlFor="address2"></label>
-              <input 
-                onChange={this.handleInfoChange}
-                type="text" 
-                id="address2"
-                value={info.address2}
-              />
-            </p>
+              <p>
+                <label htmlFor="address2"></label>
+                <input 
+                  onChange={this.handleInfoChange}
+                  type="text" 
+                  id="address2"
+                  value={info.address2}
+                />
+              </p>
 
-            <p>
-              <label htmlFor="phone">Phone:</label>
-              <input 
-                onChange={this.handleInfoChange}
-                type="text" 
-                id="phone"
-                value={info.phone}
-              />
-            </p>
+              <p>
+                <label htmlFor="phone">Phone:</label>
+                <input 
+                  onChange={this.handleInfoChange}
+                  type="text" 
+                  id="phone"
+                  value={info.phone}
+                />
+              </p>
 
-            <p>
-              <label htmlFor="email">Email:</label>
-              <input 
-                onChange={this.handleInfoChange}
-                type="text" 
-                id="email"
-                value={info.email}
-              />
-            </p>
+              <p>
+                <label htmlFor="email">Email:</label>
+                <input 
+                  onChange={this.handleInfoChange}
+                  type="text" 
+                  id="email"
+                  value={info.email}
+                />
+              </p>
 
-            <p>
-              <label htmlFor="linkedIn">LinkedIn:</label>
-              <input 
-                onChange={this.handleInfoChange}
-                type="text" 
-                id="linkedIn"
-                value={info.linkedIn}
-              />
-            </p>
+              <p>
+                <label htmlFor="linkedIn">LinkedIn:</label>
+                <input 
+                  onChange={this.handleInfoChange}
+                  type="text" 
+                  id="linkedIn"
+                  value={info.linkedIn}
+                />
+              </p>
 
-            <p>
-              <label htmlFor="gitHub">GitHub:</label>
-              <input 
-                onChange={this.handleInfoChange}
-                type="text" 
-                id="gitHub"
-                value={info.gitHub}
-              />
-            </p>
-          
-        </fieldset>
+              <p>
+                <label htmlFor="gitHub">GitHub:</label>
+                <input 
+                  onChange={this.handleInfoChange}
+                  type="text" 
+                  id="gitHub"
+                  value={info.gitHub}
+                />
+              </p>
             
-
-
+          </fieldset>
+              
         </div>
 
         <div className="resume-render">
           <Info info={info}/>
+          <Experience experiences={experiences}/>
         </div>
       </div>
     );
